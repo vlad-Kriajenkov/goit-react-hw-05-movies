1155efc2ae162128258539588d079215dfe71819
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-05-27 12:13:08 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import AppBar from 'components/AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
-import { Container } from '@mui/material';
+
 const Layout = () => {
   return (
     <div >
