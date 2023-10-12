dbe34c57b9425482442b07e7a0fc7ed1c38b2070
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-12 14:11:08 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export * from './Modal';
 export * from './InputSerchFilm';
 export * from './Credits';
 export * from './Reviews';
+export * from './PopularWeek';
+export * from './SliderTrendingFilm'
