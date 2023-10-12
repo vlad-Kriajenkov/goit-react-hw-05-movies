60d8b5c7dfa9c32bb8d1c5df16d847ebcce08a9f
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-12 14:09:55 +0300

    Fix import

--- src/components/SliderTrendingFilm/SliderTrendingFilm.jsx
+++ src/components/SliderTrendingFilm/SliderTrendingFilm.jsx
@@ -4,8 +4,7 @@ import './SliderTrendingFilm.css';
 import * as API from 'service/api';
 import { Rating } from '@mui/material';
 
-export default function SliderTrendingFilm() {
- 
+const SliderTrendingFilm = () => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original';
   const [trendingFilms, setTrendingFilms] = useState();
 
@@ -20,9 +19,10 @@ export default function SliderTrendingFilm() {
     pauseOnHover: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-   
+
+
   };
- 
+
   useEffect(() => {
     const axsiosTrending = async () => {
       const data = await API.getTrending('all/day');
@@ -31,52 +31,52 @@ export default function SliderTrendingFilm() {
     axsiosTrending();
   }, []);
 
-  return ( 
-      <Slider {...settings}>
-        {trendingFilms?.results.map(
-          ({
-            backdrop_path,
-            id,
-            title,
-            name,
-            overview,
-            release_date,
-            first_air_date,
-            vote_average,
-          }) => {
-            return (
-              <div key={id} className="slider__card">
-                <div className="wrapp">
-                  <img src={BASE_URL + backdrop_path} alt={title} />
+  return (
+    <Slider {...settings}>
+      {trendingFilms?.results.map(
+        ({
+          backdrop_path,
+          id,
+          title,
+          name,
+          overview,
+          release_date,
+          first_air_date,
+          vote_average,
+        }) => {
+          return (
+            <div key={id} className="slider__card">
+              <div className="wrapp">
+                <img src={BASE_URL + backdrop_path} alt={title} />
 
-                  <div className="slider__info">
-                    <h1 className="slider__title">
-                      {title === undefined ? name : title}
-                    </h1>
-                    <div className="slider__relize">
-                      <Rating
-                        name="read-only"
-                        max={10}
-                        value={vote_average}
-                        size="small"
-                        readOnly
-                        className="reating"
-                      />
-                      <p>
-                        {release_date === undefined
-                          ? first_air_date
-                          : release_date}
-                      </p>
-                    </div>
-
-                    <p className="slider__text">{overview}</p>
+                <div className="slider__info">
+                  <h1 className="slider__title">
+                    {title === undefined ? name : title}
+                  </h1>
+                  <div className="slider__relize">
+                    <Rating
+                      name="read-only"
+                      max={10}
+                      value={vote_average}
+                      size="small"
+                      readOnly
+                      className="reating"
+                    />
+                    <p>
+                      {release_date === undefined
+                        ? first_air_date
+                        : release_date}
+                    </p>
                   </div>
+
+                  <p className="slider__text">{overview}</p>
                 </div>
               </div>
-            );
-          }
-        )}
-      </Slider>
-  
+            </div>
+          );
+        }
+      )}
+    </Slider>
   );
-}
+};
+export {SliderTrendingFilm}
