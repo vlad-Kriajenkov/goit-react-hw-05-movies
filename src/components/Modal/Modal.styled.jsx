3d570cba8e34310476adc2a653d698d6efc07a5f
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-13 12:44:07 +0300

    Update style
    add background

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import BackGraund from 'assets/images/home/BackgroundBlurs.png';
 
 export const Blackdrop = styled.div`
   position: fixed;
@@ -9,10 +10,15 @@ export const Blackdrop = styled.div`
 
   /* overflow-x: hidden; */
   width: 100%;
-  height: 100%;
+  height: 100dvh;
 
   opacity: 1;
   background: rgb(16, 26, 35);
+  background-image: url(${BackGraund});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
   transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
   &.active {
     transform: translate(-50%, -50%);
