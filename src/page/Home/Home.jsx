4731034202e79f221c77b88d5c5fe5087e01c023
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-13 14:46:17 +0300

    Add new trending
    trendingTV

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,8 +1,45 @@
+import { useEffect, useState } from 'react';
 import { Container } from '@mui/material';
-import { PopularWeek, SliderTrendingFilm, PopularFilm } from 'components';
+import {
+  PopularWeek,
+  SliderTrendingFilm,
+  PopularFilm,
+  CardDesktop,
+  Loader,
+} from 'components';
 import { useMediaQuery } from 'react-responsive';
+import * as API from 'service/api';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const Home = () => {
   const isMobile = useMediaQuery({ query: '(max-width: 425px)' });
+  const [trendingFilms, setTrendingFilms] = useState();
+  const [trendingTV, setTrendingTV] = useState();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const axsiosTrending = async () => {
+      try {
+        const data = await API.getTrending('movie/day');
+        setTrendingFilms(data);
+        setLoading(false);
+      } catch (error) {
+        Notify.failure('Qui timide rogat docet negare');
+      }
+    };
+    const axsiosTrendingTV = async () => {
+      try {
+        const data = await API.getTrendingTV();
+        setTrendingTV(data);
+        console.log('tv=>', data);
+        setLoading(false);
+      } catch (error) {
+        Notify.failure('Qui timide rogat docet negare');
+      }
+    };
+    axsiosTrending();
+    axsiosTrendingTV();
+  }, []);
 
   return (
     <div>
@@ -16,7 +53,21 @@ const Home = () => {
         </div>
       )}
 
-      <PopularWeek />
+      <PopularWeek title={'Popular Films'}>
+        {trendingFilms?.results.map(val => (
+          <Loader loading={loading} key={nanoid()}>
+            <CardDesktop item={val} />
+          </Loader>
+        ))}
+      </PopularWeek>
+
+      <PopularWeek title={'Popular TV shows'}>
+        {trendingTV?.results.map(val => (
+          <Loader loading={loading} key={nanoid()}>
+            <CardDesktop item={val} />
+          </Loader>
+        ))}
+      </PopularWeek>
     </div>
   );
 };
