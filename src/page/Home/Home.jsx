03499b74a8735351c56c54b08cb59b8c063438b0
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-13 12:48:19 +0300

    Update Home.jsx
    Add mobile media

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,11 +1,20 @@
-import { PopularWeek, SliderTrendingFilm } from 'components';
-
+import { Container } from '@mui/material';
+import { PopularWeek, SliderTrendingFilm, PopularFilm } from 'components';
+import { useMediaQuery } from 'react-responsive';
 const Home = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 425px)' });
+
   return (
     <div>
-      <div style={{ height: '100vh' }}>
-        <SliderTrendingFilm />
-      </div>
+      {isMobile ? (
+        <Container maxWidth="xl" style={{ paddingTop: '70px' }}>
+          <PopularFilm />
+        </Container>
+      ) : (
+        <div style={{ height: '100vh' }}>
+          <SliderTrendingFilm />
+        </div>
+      )}
 
       <PopularWeek />
     </div>
