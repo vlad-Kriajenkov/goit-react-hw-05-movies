d683dc032d5403f9fb31a4c84384ba80c7782ad9
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-13 12:47:30 +0300

    Update
    add background

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,14 +1,14 @@
 import AppBar from 'Layout/AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
-
+import { Blackdrop } from './Layout.styled';
 const Layout = () => {
   return (
-    <>
+    <Blackdrop>
       <AppBar />
       <main>
         <Outlet />
       </main>
-    </>
+    </Blackdrop>
   );
 };
 export default Layout;
---
+++ src/Layout/Layout.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import BackGraund from 'assets/images/home/BackgroundBlurs.png';
+
+export const Blackdrop = styled.div`
+  min-height: 100vh;
+  background: rgb(16, 26, 35);
+  background-image: url(${BackGraund});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
