40788eccd0f910e261dd4c93b0e74120f7a77f28
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-13 14:44:40 +0300

    Update
    Add img load

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { LoaderBack } from './Loader.styled';
 
+import LoaderImg from 'assets/images/Loading/coverNotFound.png';
 const Loader = ({ children, loading }) => {
-  return <>{loading ? <div>Load</div> : <>{children}</>}</>;
+  return (
+    <>
+      {loading ? (
+        <LoaderBack>
+          <img src={LoaderImg} alt="" />
+        </LoaderBack>
+      ) : (
+        <>{children}</>
+      )}
+    </>
+  );
 };
 export { Loader };
---
+++ src/components/Loader/Loader.styled.jsx
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+export const LoaderBack = styled.div`
+  width: 200px;
+`;
