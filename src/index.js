524f5fc8e3659c75ad5af582f024af76bdcfd5c6
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-16 14:38:46 +0300

    Update
    Add SkeletonTheme

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'App';
 import './index.css';
-
+import { SkeletonTheme } from 'react-loading-skeleton';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/sain-stream'>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <SkeletonTheme baseColor="#313131" highlightColor="#525252">
+    <BrowserRouter basename="/sain-stream">
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </SkeletonTheme>
 );
