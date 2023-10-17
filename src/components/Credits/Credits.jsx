4386e9617d764891513b8028e4d00e038372e2e2
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-17 19:50:49 +0300

    Refactoring Component

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -2,13 +2,54 @@ import { Avatar } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import * as API from 'service/api';
-import { CardItem, CardList } from './Credits.stuled';
+import {
+  Title,
+  CardItem,
+  WrapName,
+  NameActor,
+  NamePerson,
+} from './Credits.stuled';
 import StatusNotification from 'components/StatusNotification/StatusNotification';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Slider from 'react-slick';
+import { useMediaQuery } from 'react-responsive';
 const Credits = () => {
   const [credits, setCredits] = useState();
   const { id } = useParams();
   const BASE_URL = 'https://image.tmdb.org/t/p/original';
+  const avatarMob = '4rem';
+  const avatrDesc = '2rem';
+  const isMobile = useMediaQuery({ query: '(max-width: 425px)' })
+    ? avatarMob
+    : avatrDesc;
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 425,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          autoplay: true,
+          autoplaySpeed: 3000,
+          arrows: false,
+          dots: false,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
 
   useEffect(() => {
     const axsiosCredits = async () => {
@@ -18,7 +59,6 @@ const Credits = () => {
       } catch (error) {
         Notify.failure('Qui timide rogat docet negare');
       }
-   
     };
     axsiosCredits();
   }, [id]);
@@ -26,19 +66,26 @@ const Credits = () => {
   return credits?.cast.length === 0 ? (
     <StatusNotification text={'Unfortunately no information :('} />
   ) : (
-    <CardList>
-      {credits?.cast.map(({ character, original_name, id, profile_path }) => (
-        <CardItem key={id}>
-          <Avatar
-            alt={original_name}
-            src={BASE_URL + profile_path}
-            sx={{ width: 100, height: 100 }}
-          />
-          <p>{character}</p>
-          <p>{original_name}</p>
-        </CardItem>
-      ))}
-    </CardList>
+    <>
+      <Title>Top Cast </Title>
+      <Slider {...settings}>
+        {credits?.cast.map(({ character, original_name, id, profile_path }) => (
+          <div key={id}>
+            <CardItem>
+              <Avatar
+                alt={original_name}
+                src={BASE_URL + profile_path}
+                sx={{ width: isMobile, height: isMobile }}
+              />
+              <WrapName>
+                <NameActor>{original_name}</NameActor>
+                <NamePerson>{character}</NamePerson>
+              </WrapName>
+            </CardItem>
+          </div>
+        ))}
+      </Slider>
+    </>
   );
 };
 export { Credits };
--- src/components/Credits/Credits.stuled.jsx
+++ src/components/Credits/Credits.stuled.jsx
@@ -1,24 +1,30 @@
 import styled from '@emotion/styled';
 
-export const CardList = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: -150px;
+export const Title = styled.h2`
+  font-size: 1.125rem;
+  margin-bottom: 1rem;
+  @media screen and (max-width: 425px) {
+    font-size: 1.5rem;
+  }
 `;
-
-export const CardItem = styled.li`
+export const CardItem = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
-
-  width: calc((100% -30px) / 6);
-
-  padding: 10px;
-  margin: 10px;
-
-  border-radius: 10px;
-  background-color: rgb(192 192 192);
-  color: #000000;
 `;
-
- 
+export const WrapName = styled.div`
+  width: 100%;
+  margin-left: 0.8rem;
+`;
+export const NameActor = styled.h3`
+  font-size: 0.7rem;
+  @media screen and (max-width: 425px) {
+    font-size: 1rem;
+  }
+`;
+export const NamePerson = styled.p`
+  font-size: 0.5rem;
+  color: #9ca4ab;
+   @media screen and (max-width: 425px) {
+    font-size: 0.8rem;
+  }
+`;
