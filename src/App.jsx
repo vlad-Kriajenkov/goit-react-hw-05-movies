ee2615a4297c539e506f8409d268862d8e4f317d
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-19 17:20:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import Layout from 'Layout/Layout';
+import { LoaderWeb } from 'Loader';
 import Home from 'page/Home/Home';
-import Movice from 'page/Movice/Movice';
+
 import MoviesID from 'page/MoviesID/MoviesID';
+import { useEffect, useState } from 'react';
 
 import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
+  const [isLoader, setIsLoader] = useState(true);
+
+  useEffect(() => {
+    document.body.style.overflow = `hidden`;
+
+    window.onload = () => {
+      setTimeout(() => {
+        setIsLoader(false);
+        document.body.style.overflow = `auto`;
+      }, 2000);
+    };
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="movies" element={<Movice />} />
-        <Route path="movies/:id/:media_type" element={<MoviesID />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies/:id/:media_type" element={<MoviesID />} />
+        </Route>
+      </Routes>
+      {isLoader ? <LoaderWeb /> : <></>}
+    </>
   );
 };
