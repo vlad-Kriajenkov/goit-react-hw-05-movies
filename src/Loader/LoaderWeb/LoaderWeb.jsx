a3088f8625f96d9a7c071669dcf82b91465856a4
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-19 18:18:33 +0300

    Add Loader

--- src/Loader/LoaderWeb/LoaderWeb.jsx
+++ src/Loader/LoaderWeb/LoaderWeb.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { LoadContainer } from './LoaderWeb.styled';
-
-const LoaderWeb = ({ isLoad }) => {
+import { Grid } from 'react-loader-spinner';
+const LoaderWeb = () => {
   const portalDiv = document.getElementById('loader-root');
   return ReactDOM.createPortal(
-    <LoadContainer>loader</LoadContainer>,
+    <LoadContainer>
+      <Grid
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="grid-loading"
+        radius="12.5"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </LoadContainer>,
     portalDiv
   );
 };
