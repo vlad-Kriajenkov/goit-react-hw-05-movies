5f0dbbe0a772de12a201c76b231539fed6c6a4b9
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-19 17:22:21 +0300

    Update and delete

--- src/Layout/AppBar/AppBar.jsx
+++ src/Layout/AppBar/AppBar.jsx
@@ -1,59 +1,18 @@
-import { useState } from 'react';
-import { useMediaQuery } from 'react-responsive';
-import {
-  Container,
-  Nav,
-  Link,
-  BtnOpen,
-  BtnClose,
-  WrapperMenuMob,
-} from './AppBar.stuled';
-import { Modal, InputSerchFilm } from 'components';
+import { Container, Nav, Link, WrapperInput } from './AppBar.stuled';
+import { InputSerchFilm } from 'components';
 import Logo from 'assets/images/Logo/Logo.svg';
 
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-
 const AppBar = () => {
-  const isMobile = useMediaQuery({ query: '(max-width: 640px)' });
-  const [targetModal, setTargetModal] = useState(false);
-
-  const handleMenu = () => {
-    setTargetModal(!targetModal);
-  };
-
-  if (isMobile) {
-    return (
-      <Container>
-        <img src={Logo} alt="Saint Stream" />
-        <BtnOpen onClick={handleMenu}>
-          <MenuIcon fontSize="large" />
-        </BtnOpen>
-
-        <Modal handleBurger={targetModal}>
-          <WrapperMenuMob>
-            <InputSerchFilm handleMenu={handleMenu} />
-            <Nav onClick={handleMenu}>
-              <Link to="/"> Home</Link>
-              <Link to="movies"> Movies</Link>
-            </Nav>
-
-            <BtnClose>
-              <CloseIcon onClick={handleMenu} fontSize="large" />
-            </BtnClose>
-          </WrapperMenuMob>
-        </Modal>
-      </Container>
-    );
-  }
   return (
     <Container>
-      <img src={Logo} alt="Saint Stream" />
       <Nav>
-        <Link to="/"> Home</Link>
-        <Link to="movies"> Movies</Link>
+        <Link to="/">
+          <img src={Logo} alt="Saint Stream" loading="lazy" />
+        </Link>
       </Nav>
-      <InputSerchFilm />
+      <WrapperInput>
+        <InputSerchFilm />
+      </WrapperInput>
     </Container>
   );
 };
--- src/Layout/AppBar/AppBar.stuled.jsx
+++ src/Layout/AppBar/AppBar.stuled.jsx
@@ -1,4 +1,3 @@
-/* Container */
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
@@ -7,72 +6,26 @@ export const Container = styled.header`
   z-index: 1;
   left: 0;
 
+  width: 100%;
+
   font-weight: 400;
   font-size: 16px;
   line-height: 19px;
 
-  display: grid;
-  grid-template-columns: 1fr 5fr 1fr;
+  display: flex;
   align-items: center;
-  padding: 5px 10px;
-
   background-color: #00000076;
-
-  @media (max-width: 1440px) {
-    grid-template-columns: 1.5fr 5fr 1fr;
-  }
-  @media (max-width: 640px) {
-    width: 100%;
-    grid-template-columns: 1fr 1fr;
-    padding: 10px 0px;
-  }
 `;
 
 export const Nav = styled.nav`
-  margin-left: 90px;
-  @media screen and (max-width: 640px) {
-    padding-top: 10px;
-    margin-left: 0px;
-  }
+  padding: 10px;
 `;
 
 export const Link = styled(NavLink)`
   display: inline-block;
   text-decoration: none;
-  padding: 15px 10px;
-  border-radius: 4px;
-  margin-right: 20px;
-
-  color: #ffffff;
-  &.active {
-    background-color: #2668c3;
-  }
-  &.active:hover {
-    color: #ffffff;
-  }
-
-  :last-child {
-    margin-right: 0px;
-  }
-  :hover {
-    color: #2668c3;
-  }
 `;
-
-// Mobile
-export const  BtnOpen = styled.div`
-  text-align: end;
-`;
-export const BtnClose = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  transform: translate(0px, 14px);
-`;
-export const WrapperMenuMob = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  padding: 30px 20px;
+export const WrapperInput = styled.nav`
+  padding: 10px;
+  margin-left: auto;
 `;
