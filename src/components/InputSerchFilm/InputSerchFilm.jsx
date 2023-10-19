9b1e446d6d0340ca52373f5aa1f8cdda29b60166
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-19 17:20:39 +0300

    Update
    Add window films .Add new style

--- src/components/InputSerchFilm/InputSerchFilm.jsx
+++ src/components/InputSerchFilm/InputSerchFilm.jsx
@@ -1,47 +1,93 @@
-import React, { useState } from 'react';
-import { Container } from './InputSerchFilm.styled';
+import React, { useEffect, useState } from 'react';
+import {
+  Container,
+  WrapperInput,
+  WrapperFilmsCard,
+  Link,
+  Poster,
+  NameFilm,
+} from './InputSerchFilm.styled';
 
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
-import { useNavigate } from 'react-router-dom';
+import { nanoid } from 'nanoid';
+import * as API from 'service/api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-const InputSerchFilm = ({ handleMenu }) => {
-  const navigate = useNavigate();
+const InputSerchFilm = () => {
+  const BASE_URL = 'https://image.tmdb.org/t/p/original';
   const [searchValue, setSearchValue] = useState('');
+  const [filmsArray, setFilmsArray] = useState();
+
+  useEffect(() => {
+    const serchFilm = async () => {
+      try {
+        const data = await API.searchFilm(searchValue);
+        console.log(data);
+        setFilmsArray(data);
+      } catch (error) {
+        Notify.failure('Qui timide rogat docet negare');
+      }
+    };
+    serchFilm();
+  }, [searchValue]);
 
   const handleChange = e => {
     setSearchValue(e.target.value);
   };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    handleMenu();
-    navigate(`/movies?name=${searchValue}`);
+  const onCloseSerch = () => {
+    setSearchValue('');
   };
-
   return (
     <Container>
-      <InputBase
-        className="search__input"
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search"
-        value={searchValue}
-        onChange={handleChange}
-      ></InputBase>
+      <WrapperInput>
+        <InputBase
+          className="search__input"
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search"
+          value={searchValue}
+          onChange={handleChange}
+        ></InputBase>
 
-      <IconButton
-        disabled={
-          searchValue === undefined || searchValue === '' ? true : false
-        }
-        type="submit"
-        onClick={handleSubmit}
-        sx={{ p: '2px' }}
-        aria-label="search"
-      >
-        <SearchIcon />
-      </IconButton>
+        <IconButton
+          disabled={
+            searchValue === undefined || searchValue === '' ? true : false
+          }
+          type="submit"
+          sx={{ p: '2px' }}
+          aria-label="search"
+        >
+          <SearchIcon />
+        </IconButton>
+      </WrapperInput>
+      {searchValue.length >= 1 && (
+        <WrapperFilmsCard onClick={onCloseSerch}>
+          {filmsArray?.results?.map(
+            ({
+              id,
+              poster_path,
+              title,
+              vote_average,
+              media_type = 'movie',
+              name,
+            }) => (
+              <Link key={nanoid()} to={`movies/${id}/${media_type}`}>
+                <Poster>
+                  <LazyLoadImage
+                    src={BASE_URL + poster_path}
+                    alt={title}
+                    effect="blur"
+                  />
+                </Poster>
+                <NameFilm> {title}</NameFilm>
+              </Link>
+            )
+          )}
+        </WrapperFilmsCard>
+      )}
     </Container>
   );
 };
--- src/components/InputSerchFilm/InputSerchFilm.styled.jsx
+++ src/components/InputSerchFilm/InputSerchFilm.styled.jsx
@@ -1,6 +1,10 @@
-const { default: styled } = require('@emotion/styled');
-
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
 export const Container = styled.div`
+  position: relative;
+`;
+
+export const WrapperInput = styled.div`
   display: flex;
   background-color: #fff;
   border-radius: 10px;
@@ -12,9 +16,56 @@ export const Container = styled.div`
     font-size: 0.7rem;
   }
   @media screen and (max-width: 425px) {
-    width: 20rem;
+    width: 14rem;
     input {
       font-size: 1rem;
     }
   }
+`;
+
+export const WrapperFilmsCard = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translateY(12.1%);
+
+  font-size: 0.7rem;
+
+  width: 14.39rem;
+  height: 360px;
+  overflow-y: scroll;
+  padding: 8px;
+  border-radius: 10px;
+  background-color: #1e1e1ef3;
+  @media screen and (max-width: 425px) {
+    top: -20%;
+    left: 50%;
+
+    width: 14rem;
+    transform: translate(-52%, 15%);
+  }
+`;
+export const Link = styled(NavLink)`
+  display: grid;
+  grid-template-columns: 0.3fr 1fr;
+
+  text-decoration: none;
+  margin-bottom: 10px;
+`;
+export const Poster = styled.div`
+  width: 2.8rem;
+
+  border: 0.5px solid white;
+  border-radius: 10px;
+  span {
+    height: fit-content;
+    img {
+      border-radius: 10px;
+    }
+  }
+`;
+
+export const NameFilm = styled.h2`
+  font-size: 0.6rem;
+  color: white;
 `;
